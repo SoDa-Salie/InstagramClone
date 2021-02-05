47c0274333da17ea6e602d441cac875173bb5843
Nikita Mager <nickita.mager@yandex.ru>
2021-02-05 23:51:37 +0700

    8.Init Firebase

--- app/src/main/java/com/example/instagramclone/HomeActivity.kt
+++ app/src/main/java/com/example/instagramclone/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.instagramclone
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeActivity : BaseActivity(0) {
@@ -12,5 +13,15 @@ class HomeActivity : BaseActivity(0) {
 		setContentView(R.layout.activity_home)
 		initBottomNavMenu()
 		Log.d(TAG, "onCreate")
+
+		val auth = FirebaseAuth.getInstance()
+		auth.signInWithEmailAndPassword("nickita.mager@yandex.ru", "148841")
+			.addOnCompleteListener {
+				if (it.isSuccessful) {
+					Log.d(TAG, "signIn: success")
+				} else {
+					Log.e(TAG, "signIn: failure", it.exception)
+				}
+			}
 	}
 }
